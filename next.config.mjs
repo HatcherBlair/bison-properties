028b6788c69c63e68c9f21807fddfff02b383745
Hatcher Blair <hatcherbb@gmail.com>
2024-04-18 11:15:03 -0600

    Added S3 packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bison-properties-file-store.s3.us-east-1.amazonaws.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
