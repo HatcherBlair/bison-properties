1c85f0f4884e8fd8848bd05752761702c79ebcfe
Hatcher Blair <hatcherbb@gmail.com>
2024-05-06 12:59:37 -0600

    Edited login/logout buttons

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,22 @@
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Link from "next/link";
 
 export default async function NavBar() {
   return (
     <nav className="flex justify-between p-4">
       <Link href="/"> Home</Link>
-      <Link href="/sign-up">Sign Up</Link>
-      <Link href="/sign-in">Sign In</Link>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
 
       <Link href="/properties">Properties</Link>
     </nav>
