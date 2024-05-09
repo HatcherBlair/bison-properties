27743f5cae32d01e8c5989e3b45ad7bb0a720b20
Hatcher Blair <hatcherbb@gmail.com>
2024-05-09 11:19:26 -0600

    Added lorem ipsum to about section

--- components/aboutSection.tsx
+++ components/aboutSection.tsx
@@ -1,8 +1,30 @@
 export default function About() {
   return (
     <section>
-      <h1>ABOUT</h1>
-      <p>This is the about section</p>
+      <h1 className="text-center font-semibold text-5xl pt-12">ABOUT</h1>
+      <p className="p-5">
+        &emsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci
+        phasellus egestas tellus rutrum tellus. Mauris cursus mattis molestie a.
+        Commodo quis imperdiet massa tincidunt nunc pulvinar. Lorem donec massa
+        sapien faucibus et. Velit dignissim sodales ut eu sem integer vitae
+        justo. Ipsum dolor sit amet consectetur adipiscing elit duis. Euismod
+        quis viverra nibh cras pulvinar mattis nunc sed blandit. Fringilla est
+        ullamcorper eget nulla facilisi etiam dignissim. Integer quis auctor
+        elit sed vulputate mi sit. Enim ut sem viverra aliquet eget sit amet.
+        Mauris in aliquam sem fringilla ut morbi. Morbi enim nunc faucibus a
+        pellentesque sit.
+      </p>
+      <p className="p-5">
+        &emsp;Velit sed ullamcorper morbi tincidunt ornare massa eget. Turpis
+        egestas maecenas pharetra convallis posuere morbi leo. Et molestie ac
+        feugiat sed lectus vestibulum. Quisque non tellus orci ac auctor augue
+        mauris. Bibendum arcu vitae elementum curabitur. Amet porttitor eget
+        dolor morbi non arcu. Cras pulvinar mattis nunc sed blandit libero. Sed
+        tempus urna et pharetra pharetra massa. Pulvinar neque laoreet
+        suspendisse interdum consectetur libero id faucibus nisl. Eu feugiat
+        pretium nibh ipsum.
+      </p>
     </section>
   );
 }
