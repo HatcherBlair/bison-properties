a88113675bf5419b1bedb7ae0440754afb9d5c0c
HatcherBlair <hatcherbb@gmail.com>
2024-05-20 14:32:56 -0600

    Updated navabr colors

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,8 +8,8 @@ export default async function NavBar() {
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-opacity-75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link className="z-40 font-semibold" href="/">
-            Home
+          <Link className="z-40 font-bold text-2xl" href="/">
+            Bison<span className="text-blue-800">Properties</span>
           </Link>
           <div className="flex h-full items-center space-x-4">
             <Link href="/properties">Properties</Link>
