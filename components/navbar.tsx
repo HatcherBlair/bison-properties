de11f92a540400a452766626e772b96e0cf8fbc2
HatcherBlair <hatcherbb@gmail.com>
2024-06-27 10:36:03 -0600

    Modify carousel to have preview of all images and enable scroll wheel interactions

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import MaxWidthWrapper from "./maxWidthWrapper";
 
 export default async function NavBar() {
   return (
-    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-opacity-75 backdrop-blur-lg transition-all">
+    <nav className="bg-slate-200 sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-opacity-75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link className="z-40 font-bold text-2xl" href="/">
